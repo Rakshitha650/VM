9c0851f3fe8f24dce95ed60f593e7d4d9d8e4388
techno-376 <rakshitham38@gmail.com>
2025-03-13 16:16:51 +0530

    [MOSIP-37385]

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "ap-south-1" # Change the region as needed
+  region = "ap-south-1"
 }
 
 variable "ami_id" {
   description = "Amazon Machine Image (AMI) ID for the EC2 instance"
-  default     = "ami-023a307f3d27ea427" # Replace with your desired AMI (Ubuntu/Debian)
+  default     = "ami-023a307f3d27ea427"
 }
 
 variable "instance_type" {
@@ -14,7 +14,7 @@ variable "instance_type" {
 
 variable "key_name" {
   description = "SSH key name"
-  default     = "mosip-qa" # Replace with your key pair name
+  default     = "mosip-qa"
 }
 
 # Lookup existing security group by name
@@ -23,10 +23,12 @@ data "aws_security_group" "existing_perf_vm_sg" {
     name   = "group-name"
     values = ["mosip-k8s-performance-vm"]
   }
+  # Avoid failing if the security group doesn't exist
+  ignore_errors = true
 }
 
 resource "aws_security_group" "perf_vm_sg" {
-  count       = data.aws_security_group.existing_perf_vm_sg.id != "" ? 0 : 1
+  count       = (try(data.aws_security_group.existing_perf_vm_sg.id, "") == "") ? 1 : 0
   name        = "mosip-k8s-performance-vm"
   description = "Allow necessary access"
 
@@ -34,42 +36,23 @@ resource "aws_security_group" "perf_vm_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Change to restrict access
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 5900
     to_port     = 5900
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # For VNC access
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 51820
     to_port     = 51820
     protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"] # For WireGuard VPN
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "performance_vm" {
-  ami             = var.ami_id
-  instance_type   = var.instance_type
-  key_name        = var.key_name
-
-  vpc_security_group_ids = data.aws_security_group.existing_perf_vm_sg.id != "" ? [data.aws_security_group.existing_perf_vm_sg.id] : [aws_security_group.perf_vm_sg[0].id]
-
-  # Install required software packages
-  user_data = file("install.sh")
-
-  tags = {
-    Name = "Performance-VM"
-  }
-}
+    t
