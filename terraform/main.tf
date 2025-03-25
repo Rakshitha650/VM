5602426196e6a607ca1646b38ed0ec880e10cc33
techno-376 <rakshitham38@gmail.com>
2025-03-25 10:50:42 +0530

    [MOSIP-37385]

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-south-1" # Change the region as needed
 }
 
 variable "ami_id" {
   description = "Amazon Machine Image (AMI) ID for the EC2 instance"
-  default     = "ami-023a307f3d27ea427"
+  default     = "ami-023a307f3d27ea427" # Replace with your desired AMI (Ubuntu/Debian)
 }
 
 variable "instance_type" {
@@ -14,41 +14,46 @@ variable "instance_type" {
 
 variable "key_name" {
   description = "SSH key name"
-  default     = "mosip-qa"
+  default     = "mosip-qa" # Replace with your key pair name
 }
 
-# Lookup existing security group by name
-data "aws_security_group" "existing_perf_vm_sg" {
-  filter {
-    name   = "group-name"
-    values = ["mosip-k8s-performance-vm"]
+resource "aws_instance" "performance_vm" {
+  ami             = var.ami_id
+  instance_type   = var.instance_type
+  key_name        = var.key_name
+  security_groups = [aws_security_group.perf_vm_sg.name]
+
+  # Install required software packages
+  user_data = file("install.sh")
+
+  tags = {
+    Name = "Performance-VM"
   }
 }
 
 resource "aws_security_group" "perf_vm_sg" {
-  count       = (try(data.aws_security_group.existing_perf_vm_sg.id, "") != "" ? 0 : 1)
-  name        = "mosip-k8s-performance-vm"
+  name        = "mosip-k8s-performance-vm5"
   description = "Allow necessary access"
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # Change to restrict access
   }
 
   ingress {
     from_port   = 5900
     to_port     = 5900
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # For VNC access
   }
 
   ingress {
     from_port   = 51820
     to_port     = 51820
     protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # For WireGuard VPN
   }
 
   egress {
@@ -56,22 +61,6 @@ resource "aws_security_group" "perf_vm_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "performance_vm" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
-  vpc_security_group_ids = try(data.aws_security_group.existing_perf_vm_sg.id, "") != "" ?
-    [data.aws_security_group.existing_perf_vm_sg.id] :
-    [aws_security_group.perf_vm_sg[0].id]
-
-  user_data = file("install.sh")
-
-  tags = {
-    Name = "Performance-VM"
   }
 }
 
