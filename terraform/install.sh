38e8af0b5a6e7e2b9b900954ede59adc377a7b85
techno-376 <rakshitham38@gmail.com>
2025-03-27 12:03:40 +0530

    [MOSIP-37385]

--- terraform/install.sh
+++ terraform/install.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+
+# Log file path
+echo "[ Set Log File ] : "
+LOG_FILE="/tmp/performance-vm-$( date +"%d-%h-%Y-%H-%M" ).log"
+
+# Redirect stdout and stderr to log file
+exec > >(tee -a "$LOG_FILE") 2>&1
+
+# Set commands for error handling.
 set -e
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o nounset   ## set -u : exit the script if you try to use an uninitialized variable
+set -o errtrace  # Trace ERR through 'time command' and other functions
+set -o pipefail  # Trace ERR through pipes
 
 # Update package list and install dependencies
 sudo apt update -y && sudo apt upgrade -y
